088a3ca181074e9af9a49008057ecc1110571d20
MaryamTariq-1 <marymughal216@gmail.com>
2024-10-27 02:13:33 +0500

    Environment-Setup

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,14 @@
 const express = require("express");
+
+const app = express()
+
+app.get(
+    "/",
+    (req,res) => (
+        res.send("Hello")
+    )
+)
+app.listen(
+    5000,
+    () => console.log("Backend is running")
+)
