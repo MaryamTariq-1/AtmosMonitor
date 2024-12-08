70a13e099d81ce41e0308b4d882db36255dacea2
Razeen Shahid <shahidrazeen@mail.com>
2024-12-09 00:13:51 +0500

    Google Earth Engine: Adding buffer layers

--- backend/predictionModel/gee_dataset.js
+++ backend/predictionModel/gee_dataset.js
@@ -2,3 +2,9 @@
 var Faisalabad = ee.Geometry.Point([73.079109, 31.418715]); // Define the central point for Faisalabad
 var FaisalabadRegion = Faisalabad.buffer(20000); // 20 km buffer around Faisalabad
 
+// Center the map over Faisalabad region
+Map.centerObject(FaisalabadRegion, 10); // Zoom level 10
+
+// Add a layer to highlight Faisalabad region with a buffer
+Map.addLayer(FaisalabadRegion, {color: 'red'}, 'Faisalabad Region');
+
